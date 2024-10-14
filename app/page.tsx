1ed7ccb5e8bddf669bab94b1208d0d09cd17e8e9
rupesh00mahat <rupesh00mahat@gmail.com>
2024-10-14 15:11:37 +0545

    create basic layout, playlists, current song, play, pause next, prev functionalities

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,121 @@
-import Image from "next/image";
+"use client";
+import { FC, useEffect, useState } from "react";
+import { FaMusic, FaPause, FaPlay } from "react-icons/fa";
+import { MdSkipNext, MdSkipPrevious } from "react-icons/md";
 
-export default function Home() {
+const music = [
+  "music/arjun-vailay.mp3",
+  "music/jale-2.mp3",
+  "music/pehle-bhi-main.mp3",
+  "music/ram-siya-ram.mp3",
+];
+
+const Home: FC = () => {
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
+  const [duration, setDuration] = useState<number>(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+     console.log(audio?.currentTime);
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    if (audio) {
+      audio.pause();
+    }
+    const newAudio = new Audio(music[currentSongIndex]);
+    setAudio(newAudio);
+    setIsPlaying(false);
+  }, [currentSongIndex]);
+
+  const playPause = () => {
+    if (audio) {
+      if (isPlaying) {
+        audio.pause();
+      } else {
+        audio.play();
+      }
+      setIsPlaying(!isPlaying);
+    }
+    console.log(duration);
+  };
+
+  const skipNext = () => {
+    setCurrentSongIndex((prevIndex) =>
+      prevIndex === music.length - 1 ? 0 : prevIndex + 1
+    );
+    setIsPlaying(false);
+  };
+
+  const skipPrevious = () => {
+    setCurrentSongIndex((prevIndex) =>
+      prevIndex === 0 ? music.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main>
+      <h2 className="text-3xl text-center mt-5 mb-12">Next Music</h2>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-[70%_30%] gap-5">
+          <div className="current-music border p-5 rounded-md h-10/12">
+            <div className="music-icon-wrapper flex justify-center items-center">
+              <FaMusic size={200} />
+            </div>
+            <div className="playbar-area mt-40 flex flex-col items-center justify-between border p-3 gap-5 rounded-md">
+              <h4 className="text-md font-semibold ">
+                {music[currentSongIndex]}
+              </h4>
+              <div className="progress-bar h-1 relative rounded-md bg-gray-500 w-full after:absolute after:bg-red-500 after:top-0 after:left-0 after:w-5 after:h-full"></div>
+              <div className="playbar-wrapper flex items-center justify-center gap-3">
+                <button onClick={skipPrevious}>
+                  <MdSkipPrevious size={25} />
+                </button>
+                <button onClick={playPause}>
+                  {isPlaying ? <FaPause size={25} /> : <FaPlay size={25} />}
+                </button>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+                <button onClick={skipNext}>
+                  <MdSkipNext size={25} />
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="playlist border p-3 rounded-md">
+            <h4 className="text-4xl text-center">
+              {music &&
+                music.map((musicItem, index) => (
+                  <div
+                    key={`${musicItem}-${index}`}
+                    className={`song-item flex items-center justify-between border rounded-sm p-2 ${
+                      currentSongIndex === index ? "bg-gray-100" : ""
+                    } `}
+                  >
+                    <div className="music-logo-wrapper p-3 rounded-full border flex justify-center items-center">
+                      <FaMusic size={15} />
+                    </div>
+                    <h6 className="playlist-song-title text-base">
+                      {musicItem}
+                    </h6>
+                    <button
+                      onClick={() => {
+                        setCurrentSongIndex(index);
+                      }}
+                    >
+                      <FaPlay size={15} />
+                    </button>
+                  </div>
+                ))}
+            </h4>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </main>
   );
-}
+};
+
+export default Home;
